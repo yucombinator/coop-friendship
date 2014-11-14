f476c4d6eabacce170da6e3a2eb01e559681cdc0
DeonHua <deonhbci@gmail.com>
2014-11-14 18:47:09 -0500

    Hello/Bye World

--- coop-friendship.py
+++ coop-friendship.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/test')
+def nye_world():
+    return 'Bye World!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
