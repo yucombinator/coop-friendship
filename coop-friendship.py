7f3268f5247d3049b79afa759262ba0d27aa3c33
DeonHua <deonhbci@gmail.com>
2014-11-14 21:53:09 -0500

    Logic rechange

--- coop-friendship.py
+++ coop-friendship.py
@@ -1,14 +1,35 @@
 from flask import Flask, render_template
+import data
 import datetime
 app = Flask(__name__)
 
+def generateTerms(length):
+    #CREATE ARRAY OF TERMS
+    #Keeping track of dates.
+    date = str(datetime.date.today()).split("-")
+    for element in range(len(date)):
+        date[element] = int(date[element])
 
+    #Append term to array
+    terms = []
+    for index in range(length):
+        currentTerm = str(date[0])
+        if 1 <= date[1] <= 4:
+            currentTerm += "W"
+        elif 5 <= date[1] <= 8:
+            currentTerm += "S"
+        else:
+            currentTerm += "F"
+        terms.append(currentTerm)
 
-eng = {"8": ["1A", "1B", "COOP1", "2A", "COOP2", "2B", "COOP3", "3A", "COOP4", "3B", "COOP5", "4A", "COOP6", "4B"],
-       "4": ["1A", "COOP1", "1B", "COOP2","2A", "COOP3", "2B", "COOP4", "3A", "COOP5", "3B", "COOP6", "4A", "4B"],
-       "ARCH": ["1A", "1B", "OFF", "2A", "COOP1", "2B", "COOP2", "3A", "COOP3", "3B", "COOP4", "COOP5", "4A", "COOP6", "4B"]}
+        #Shift to next term and wrap
+        date[1] += 4
+        if date[1] > 12:
+            date[0] += 1
+            date[1] -= 12
+    return terms
 
-def compareArrays(array1, array2, term1="1B", term2="1A"):
+def compareArrays(array1, array2, term1="1A", term2="1A"):
     #SHIFTING THE ARRAYS
     array1 = array1[array1.index(term1):]
     array2 = array2[array2.index(term2):]
@@ -29,29 +50,8 @@ def compareArrays(array1, array2, term1="1B", term2="1A"):
     for index in range(padding):
         results.append("NO MATCH")
 
-    #CREATE ARRAY OF TERMS
-    #Keeping track of dates.
-    date = str(datetime.date.today()).split("-")
-    for element in range(len(date)):
-        date[element] = int(date[element])
+    terms = generateTerms(max(len(array1), len(array2)))
 
-    #Append term to array
-    terms = []
-    for index in range(length + padding):
-        currentTerm = str(date[0])
-        if 1 <= date[1] <= 4:
-            currentTerm += "W"
-        elif 5 <= date[1] <= 8:
-            currentTerm += "S"
-        else:
-            currentTerm += "F"
-        terms.append(currentTerm)
-
-        #Shift to next term and wrap
-        date[1] += 4
-        if date[1] > 12:
-            date[0] += 1
-            date[1] -= 12
 
 
     print(terms)
@@ -60,4 +60,4 @@ def compareArrays(array1, array2, term1="1B", term2="1A"):
     print(results)
 
 
-compareArrays(eng["8"], eng["4"], "2A", "1A")
+compareArrays(data.AFM_1, data.Biomed_eng, "1A", "1A")
