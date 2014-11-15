95f375ade35ff67e28e8259429b8b0d799fe2986
Yu Chen Hou <houyuchen66@gmail.com>
2014-11-15 10:01:29 -0500

    heroku

--- app.py
+++ app.py
@@ -92,5 +92,5 @@ if __name__ == "__main__":
     for row in csv_f:
       color = parseColor(row[1])
       programs.append({'name':row[0],'faculty':row[1],'require_stream':row[2],'color':color})
-    app.run(debug=True,host='localhost',port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get("PORT", 5000)))
 
