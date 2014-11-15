cadcf1ae79883ff6a51271f9149903b12142205f
DeonHua <deonhbci@gmail.com>
2014-11-14 20:23:52 -0500

    New web stuff

--- coop-friendship.py
+++ coop-friendship.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def arrayTest():
+    return str(2)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
 @app.route('/test')
 def nye_world():
@@ -19,5 +21,14 @@ def inner():
 def inner2():
     return "The second inside."
 
+@app.route('/array')
+def test():
+    return arrayTest()
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+dict = {"8":["1A", "1B", "COOP", "2A", "OFF"]}
+def compareArrays(array1, array2):
+
+    pass
