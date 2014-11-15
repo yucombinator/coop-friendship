5928a737435154f3df271c39830732fe569a9dfb
DeonHua <deonhbci@gmail.com>
2014-11-14 22:19:23 -0500

    Converted to Dictionary.

--- data.py
+++ data.py
@@ -1,24 +1,23 @@
-
-H_Promotion = ['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', 'COOP5', '4A', '4B']
-H_Studies = ['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', 'COOP5', '4A', '4B']
-Kinesiology = ['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', 'COOP5', '4A', '4B']
-RLS = ['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', '4A', 'COOP5', '4B']
-
-AFM = {"1":['1A', '1B', 'OFF', '2A', 'COOP1', '2B', '3A', 'COOP2', '3B', 'COOP3', '4A', 'COOP4', '4B'],
-       "2":['1A', '1B', 'OFF', '2A', 'COOP1', '2B', '3A', 'COOP2', '3B', '4A', 'COOP3', 'COOP4', '4B'],
-       "3":['1A', '1B', 'OFF', '2A', 'COOP1', '2B', '3A', 'COOP2', '3B', 'COOP3', 'COOP4', '4A', '4B']
+AHS = {"HPROMO":['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', 'COOP5', '4A', '4B'],
+       "HSTUDY":['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', 'COOP5', '4A', '4B'],
+       "KIN":['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', 'COOP5', '4A', '4B'],
+       "RLS":['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', '4A', 'COOP5', '4B']
 }
 
-Anthro = ['1A', '1B', 'OFF', '2A', '2B', 'COOP1', '3A', 'COOP2', '3B', 'COOP3', '4A', 'COOP4', '4B', ]
-Arts = ['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', 'COOP5', '4A', '4B' ]
-Arts_Spec = ['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', 'COOP5', '4A', '4B']
-CFM = ['1A', '1B', 'COOP1', '2A', 'COOP2', '2B', 'COOP3', '3A', 'COOP4', '3B', 'COOP5', '4A', 'COOP6', '4B']
-Econ = ['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', '4A', 'COOP6', '4B']
-English = ['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', 'COOP5', '4A', '4B']
+ARTS = {"AFM1":['1A', '1B', 'OFF', '2A', 'COOP1', '2B', '3A', 'COOP2', '3B', 'COOP3', '4A', 'COOP4', '4B'],
+        "AFM2":['1A', '1B', 'OFF', '2A', 'COOP1', '2B', '3A', 'COOP2', '3B', '4A', 'COOP3', 'COOP4', '4B'],
+        "AFM3":['1A', '1B', 'OFF', '2A', 'COOP1', '2B', '3A', 'COOP2', '3B', 'COOP3', 'COOP4', '4A', '4B'],
+        "ANTHRO":['1A', '1B', 'OFF', '2A', '2B', 'COOP1', '3A', 'COOP2', '3B', 'COOP3', '4A', 'COOP4', '4B'],
+        "CFM":['1A', '1B', 'COOP1', '2A', 'COOP2', '2B', 'COOP3', '3A', 'COOP4', '3B', 'COOP5', '4A', 'COOP6', '4B'],
+        "ECON":['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', '4A', 'COOP6', '4B'],
+        "ENGLISH":['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', 'COOP5', '4A', '4B']
+        "GEN":['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', 'COOP5', '4A', '4B'],
+        "GENSPEC":['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', 'COOP5', '4A', '4B']
+}
 
 ENG = {
     "ARCH":['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', 'COOP5', '4A', 'COOP6', '4B'],
-    "BIO":['1A', '1B', 'COOP1', '2A', 'COOP2', '2B', 'COOP3', '3A', 'COOP4', '3B', 'COOP5', 'COOP6', '4A', '4B'],
+    "BIOMED":['1A', '1B', 'COOP1', '2A', 'COOP2', '2B', 'COOP3', '3A', 'COOP4', '3B', 'COOP5', 'COOP6', '4A', '4B'],
     "ECE4":['1A', 'COOP1', '1B', 'COOP2', '2A', 'COOP3', '2B', 'COOP4', '3A', 'COOP5', '3B', '4A', 'COOP6', '4B'],
     "NANO":['1A', '1B', 'COOP1', '2A', 'COOP2', '2B', 'COOP3', 'COOP4', '3A', '3B', 'COOP5', 'COOP6', '4A', '4B'],
     "SE":['1A', '1B', 'COOP1', '2A', 'COOP2', '2B', 'COOP3', '3A', 'COOP4', '3B', 'COOP5', '4A', 'COOP6', '4B'],
@@ -26,18 +25,21 @@ ENG = {
     "8":['1A', '1B', 'COOP1', '2A', 'COOP2', '2B', 'COOP3', '3A', 'COOP4', '3B', 'COOP5', '4A', 'COOP6', '4B']
 }
 
-Env_B = ['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', 'COOP5', '4A', '4B']
-Env_Res = ['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', 'COOP5', '4A', '4B']
-GEM = ['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', '4A', 'COOP5', '4B']
-Geomatics = ['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', '4A', 'COOP5', '4B']
-Planning = ['1A', '1B', 'OFF', '2A', '2B', 'COOP1', '3A', 'COOP2', '3B', 'COOP3', '4A', 'COOP4', 'COOP5', '4B']
+ENV = {"BUSINESS":['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', 'COOP5', '4A', '4B'],
+       "RESOURCE":['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', 'COOP5', '4A', '4B'],
+       "GEMGMT":['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', '4A', 'COOP5', '4B'],
+       "GEOMATICS":['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', '4A', 'COOP5', '4B'],
+       "PLANNING":['1A', '1B', 'OFF', '2A', '2B', 'COOP1', '3A', 'COOP2', '3B', 'COOP3', '4A', 'COOP4', 'COOP5', '4B']
+}
 
-Biochem = ['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', 'COOP5', '4A', '4B']
-Biotech = ['1A', '1B', 'OFF', '2A', 'COOP1', '2B', '3A', 'COOP2', '3B', 'COOP3', '4A', 'COOP4', '4B']
-Materials = ['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', 'COOP4', '3B', 'COOP5', '4A', '4B']
-Physics = ['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', 'COOP3', '3A', 'COOP4', '3B', 'COOP5', '4A', '4B']
-Psy = ['1A', '1B', 'OFF', '2A', '2B', 'COOP1', '3A', 'COOP2', '3B', 'COOP3', 'COOP4', 'COOP5', '4B']
-Pharmacy = ['OFF ', '1A', '1B', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', '4A', '4B']
+
+SCI = {"BIOC":['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', 'COOP5', '4A', '4B'],
+       "BIOT":['1A', '1B', 'OFF', '2A', 'COOP1', '2B', '3A', 'COOP2', '3B', 'COOP3', '4A', 'COOP4', '4B'],
+       "MAT":['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', 'COOP4', '3B', 'COOP5', '4A', '4B'],
+       "PHYS":['1A', '1B', 'OFF', '2A', 'COOP1', '2B', 'COOP2', 'COOP3', '3A', 'COOP4', '3B', 'COOP5', '4A', '4B'],
+       "PSYCH":['1A', '1B', 'OFF', '2A', '2B', 'COOP1', '3A', 'COOP2', '3B', 'COOP3', 'COOP4', 'COOP5', '4B'],
+       "PHARM":['OFF ', '1A', '1B', '2A', 'COOP1', '2B', 'COOP2', '3A', 'COOP3', '3B', '4A', '4B']
+}
 
 MATH = {"1":['1A', '1B', 'COOP1', '2A', 'COOP2', '2B', 'COOP3', '3A', 'COOP4', '3B', 'COOP5', '4A', 'COOP6', '4B'],
         "2":['1A', '1B', 'COOP1', '2A', '2B', 'COOP2', '3A', 'COOP3', '3B', 'COOP4', '4A', 'COOP5', 'COOP6', '4B'],
